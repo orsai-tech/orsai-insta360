de241b5e6d16adfaa340ce1b2e762e3e00bf1a95
GonzaRJ <rodriguez45984@estudiantes.untref.edu.ar>
2023-06-21 12:42:31 -0300

    #7 Agregando servicios de inicio de RPi

---
+++ Servicios/kiosk.sh
@@ -0,0 +1,12 @@
+sleep 1
+
+xset s noblank
+xset s off
+xset -dpms
+unclutter -idle -roo &
+
+
+ffplay -hide_banner -loglevel error -f v4l2 -framerate 30 -input_format mjpeg -video_size hd720 /dev/video0
+
+DISPLAY=:0 xdotool search --onlyvisible --name "/dev/video0" windowsize 100% 100% && xrandr --output HDMI-1 --auto
+
